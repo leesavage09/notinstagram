6ad6aedfb38c2ed261163d207e50b2e7acedf527
leesavage09 <leesavage09@gmail.com>
2020-04-17 15:41:14 +0100

    Cross-Origin Resource Sharing (CORS) -init

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt'
 
+gem 'rack-cors'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,12 @@ module Notinstagram
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
