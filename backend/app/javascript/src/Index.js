0ab024b84b4ffa15b92c92e4fdc42632d19f44b6
leesavage09 <leesavage09@gmail.com>
2020-04-19 17:50:28 +0100

    moved react src and scss - init

--- backend/app/javascript/src/Index.js
+++ backend/app/javascript/src/Index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './redux/store'
 import App from './App';
+import './index'
 
 
 class Index extends React.Component {
