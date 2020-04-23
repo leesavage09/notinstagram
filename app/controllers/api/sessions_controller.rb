e4d7307cc60f3f84873c3884c38216d3c616948a
leesavage09 <leesavage09@gmail.com>
2020-04-23 13:10:53 +0100

    jbuilder views

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,19 +1,20 @@
 class Api::SessionsController < ApplicationController
 
     def create
-        user = User.find_by_credentials(user_params[:username],user_params[:password])
-        if user 
-            render json: login(user) #TODO render secure JSON 
+        @user = User.find_by_credentials(user_params[:username],user_params[:password])
+        if @user 
+            login(@user)
+            render :show 
         else
             render json: {errors: {auth: ['username or password incorect']}}, status: 401
         end 
     end
 
     def destroy
-        user = self.logged_in_user
-        if user
+        @user = self.logged_in_user
+        if @user
             logout
-            render json: user  #TODO render secure JSON
+            render :show
         else 
             render json:  {errors: {auth: ['already logged out']}}, status: 404
         end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,12 +3,12 @@ class Api::UsersController < ApplicationController
   before_action :require_user_logged_in, except: [:create]
   
   def create
-      user = User.new(user_params)
-      if user.save
-          render json: user
-          login(user)
+      @user = User.new(user_params)
+      if @user.save
+          login(@user)  
+          render :show
         else
-          render json: {errors: user.errors.messages}, status: :unprocessable_entity
+          render json: {errors: @user.errors.messages}, status: :unprocessable_entity
       end
   end
   
@@ -16,9 +16,10 @@ class Api::UsersController < ApplicationController
       if !logged_in? || logged_in_user.id != params.require(:id).to_i
         render json: {errors: {auth: ['You must be logged in']}} , status: :forbidden
       else
-        logged_in_user.update(user_params)
-        if logged_in_user.save
-            render json: logged_in_user
+        @user = logged_in_user
+        @user.update(user_params)
+        if @user.save
+            render :show
           else
             render json: {errors: logged_in_user.errors.messages}, status: :unprocessable_entity
         end
