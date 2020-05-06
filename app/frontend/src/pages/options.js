9810b7e9d3d0ac542902d02e593d7bbaccd26f3e
leesavage09 <leesavage09@gmail.com>
2020-05-06 13:28:10 +0100

    options page styled

--- app/frontend/src/pages/options.js
+++ app/frontend/src/pages/options.js
@@ -1,14 +1,41 @@
-import { useSelector, useDispatch } from 'react-redux'
 import React from 'react';
 import TopNav from '../components/top_nav/top_nav_options'
 import BottomNav from '../components/mobile_footer'
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import { logout } from '../redux/actions/session_actions'
 
 export default function Options() {
+    const dispatch = useDispatch();
+    function logoutClicked() {
+        dispatch(logout())
+    }
 
     return (
-        <div>
+        <div className='options'>
             <TopNav />
-            Options
+            <div className='options__body'>
+                <h3 className='options__title'>ACCOUNT</h3>
+                <Link to='/accounts/edit' className='options__link'>
+                    <div className='options__text'>Edit Profile</div>
+                    <div className='right-chevron options__chevron'></div>
+                </Link>
+
+                <Link to='' className='options__link'>
+                    <div className='options__text'>Change Password</div>
+                    <div className='right-chevron options__chevron'></div>
+                </Link>
+            </div>
+
+            <div className='options__body'>
+                <h3 className='options__title'></h3>
+
+                <a onClick={logoutClicked} className='options__link'>
+                    <div className='options__text'>Log Out</div>
+                    <div className='right-chevron options__chevron'></div>
+                </a>
+            </div>
+
             <BottomNav />
         </div>
     );
